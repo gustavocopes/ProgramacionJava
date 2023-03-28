81664b4f3f51315afbad92abf16d0cbacc1b6df5
Gustavo Copes <gcopesserradell@gmail.com>
2023-03-27 22:00:00 -0300

    Primer clase java

---
+++ Ejercicio1_lectura1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1_lectura1;
+
+/**
+ *
+ * @author HP
+ */
+public class Ejercicio1_lectura1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String apellido = "rosso";
+        int dia = 12;
+        double tiempo = 3.556;
+        char color = 'a';
+        boolean val;
+        byte binario = 1;
+        float num =  3.14f;
+       
+        double resultado;
+        resultado = tiempo + dia;
+        
+        val =  binario > num;
+        
+        float decimal = 2.47f + num;
+        
+        System.out.println("apellido:"  + apellido);
+        
+    }
+    
+}
