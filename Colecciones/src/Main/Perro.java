aaad394fc8f67b7e575c505002fb7531a7350b97
Gustavo Copes <gcopesserradell@gmail.com>
2023-05-16 14:42:47 -0300

    Ej 2 Colecciones Perro

--- Colecciones/src/Main/Perro.java
+++ Colecciones/src/Main/Perro.java
@@ -2,12 +2,14 @@ package Main;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import servicios.PerroServicio;
 
 /*
  *Diseñar un programa que lea y guarde razas de perros en un ArrayList de tipo String. El
 programa pedirá una raza de perro en un bucle, el mismo se guardará en la lista y
 después se le preguntará al usuario si quiere guardar otro perro o si quiere salir. Si decide
 salir, se mostrará todos los perros guardados en el ArrayList.
+
  */
 
 /**
@@ -18,19 +20,12 @@ public class Perro {
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> perros = new ArrayList();
-        String opcion = "n";
+       
+        PerroServicio ps = new PerroServicio();
         
-        do{
-        System.out.println("Ingrese la raza del perro: ");
-        String raza = sc.next();
-        perros.add(raza);
-            System.out.println("Desea cargar otro perro? s/n");
-            opcion = sc.next();
-        
-        }while(opcion.equalsIgnoreCase("s"));
-        
+        ps.crearPerro();
+        ps.mostrarPerro();
+        ps.eliminarPerro();
     }
 
 }
---
+++ Colecciones/src/servicios/PerroServicio.java
@@ -0,0 +1,62 @@
+/*
+ Continuando el ejercicio anterior, después de mostrar los perros, al usuario se le pedirá
+un perro y se recorrerá la lista con un Iterator, se buscará el perro en la lista. Si el perro
+está en la lista, se eliminará el perro que ingresó el usuario y se mostrará la lista
+ordenada. Si el perro no se encuentra en la lista, se le informará al usuario y se mostrará
+la lista ordenada..
+ */
+package servicios;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class PerroServicio {
+     ArrayList<String> perros = new ArrayList();
+     Scanner sc = new Scanner(System.in);
+       
+    public void crearPerro(){
+      
+        String opcion = "n";
+        
+        do{
+        System.out.println("Ingrese la raza del perro: ");
+        String raza = sc.next();
+        perros.add(raza);
+            System.out.println("Desea cargar otro perro? s/n");
+            opcion = sc.next();
+        
+        }while(opcion.equalsIgnoreCase("s"));
+    }
+    
+    public void mostrarPerro(){
+        
+        System.out.println("Las razas de los perros son: ");
+        for (String aux : perros){
+            System.out.println(aux);
+        }
+        
+    }
+    
+    public void eliminarPerro(){
+        System.out.println("Ingrese una raza de perro a eliminar de la lista: ");
+        String op = sc.next();
+        Iterator<String> it = perros.iterator();
+        while (it.hasNext()){
+            if (it.next().equalsIgnoreCase(op)){
+                it.remove();
+            }
+        }
+        
+        System.out.println("Desea ingresar otro perro? s/n");
+        String op1 = sc.next();
+        if (op1.equalsIgnoreCase("s")) {
+            crearPerro();
+                    }
+        
+    }
+}
