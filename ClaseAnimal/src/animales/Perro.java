ece8cabaefb853b73bd058219dbb96d11c3c35ec
Gustavo Copes <gcopesserradell@gmail.com>
2023-06-15 14:16:10 -0300

    Ej 1 Herencia Animal

---
+++ ClaseAnimal/src/animales/Perro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animales;
+
+/**
+ *
+ * @author GCopes
+ */
+public class Perro extends Animal{
+
+    public Perro(String nombre, int edad, String alimento, String raza) {
+        super(nombre, edad, alimento, raza);
+    }
+
+    public Perro() {
+    }
+    
+    
+    
+}
